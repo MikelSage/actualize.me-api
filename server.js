6159d3059105e79151bad9d7cc64b7f9397804f3
MikelSage <michael.sagapolutele@yahoo.com>
2017-10-28 12:44:43 -0600

    add github-oath, dotenc packages

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
 app.locals.title = 'actualize-api'
 app.set('port', process.env.PORT || 3000)
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (request, response) => {
-  response.send('It is secret')
+  response.send(process.env.USERNE)
 })
 
 if (!module.parent) {
