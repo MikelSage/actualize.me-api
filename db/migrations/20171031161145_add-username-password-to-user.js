a5582eef5956c3a97ab79e86fec2255a0d1ef0b7
MikelSage <michael.sagapolutele@yahoo.com>
2017-10-31 16:14:17 -0600

    write migration to add username/password to user

---
+++ db/migrations/20171031161145_add-username-password-to-user.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('users', function(table) {
+    table.string('username')
+    table.string('password')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('users', function(table) {
+    table.dropColumns('username', 'password')
+  })
+};
