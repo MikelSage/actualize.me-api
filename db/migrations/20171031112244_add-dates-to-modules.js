ab90d9c05d5f876fa2f148ac9baa66f18c90b4b0
MikelSage <michael.sagapolutele@yahoo.com>
2017-10-31 11:27:23 -0600

    write migration to add start and end date to modules table

---
+++ db/migrations/20171031112244_add-dates-to-modules.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('modules', function(table) {
+    table.dateTime('start_date')
+    table.dateTime('end_date')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('modules', function(table) {
+    table.dropColumn('start_date')
+    table.dropColumn('end_date')
+  })
+};
