3889eae65f066739031078f6ea4a0b54d6f23034
MikelSage <michael.sagapolutele@yahoo.com>
2017-11-05 15:44:10 -0700

    write migration to add submissions table

---
+++ db/migrations/20171105153314_create-submissions-table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('submissions', function(table) {
+    table.increments('id').primary()
+    table.string('notes')
+    table.string('github_url')
+    table.integer('user_id').unsigned().references('id').inTable('users')
+    table.integer('project_id').unsigned().references('id').inTable('projects')
+    table.timestamps(true, true)
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('submissions')
+};
