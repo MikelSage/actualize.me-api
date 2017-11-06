719060ae9f7dddf2668e329f1626af7ede4239f9
MikelSage <michael.sagapolutele@yahoo.com>
2017-11-05 17:21:18 -0700

    create scores table

---
+++ db/migrations/20171105171728_create-scores-table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('scores', function(table) {
+    table.increments('id').primary()
+    table.integer('score')
+    table.integer('area_id').unsigned().references('id').inTable('areas')
+    table.integer('submission_id').unsigned().references('id').inTable('submissions')
+    table.timestamps(true, true)
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('scores')
+};
